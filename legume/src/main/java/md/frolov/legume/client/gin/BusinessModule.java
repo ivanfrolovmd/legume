ef4219cc3d6c42abe3b2fd89b2d538d393d2a982
Ivan Frolov <ivan@frolov.md>
2013-05-31 12:53:36 +0300

    Cleanup

--- legume/src/main/java/md/frolov/legume/client/gin/BusinessModule.java
+++ legume/src/main/java/md/frolov/legume/client/gin/BusinessModule.java
@@ -1,11 +1,9 @@
 package md.frolov.legume.client.gin;
 
 import com.google.gwt.inject.client.AbstractGinModule;
-import com.google.inject.Provides;
 
 import md.frolov.legume.client.elastic.ElasticSearchService;
 import md.frolov.legume.client.elastic.impl.ElasticSearchServiceImpl;
-import md.frolov.legume.client.elastic.model.ModelFactory;
 import md.frolov.legume.client.service.ConfigurationService;
 import md.frolov.legume.client.service.impl.ConfigurationServiceImpl;
 import md.frolov.legume.client.util.ColorUtils;
@@ -18,10 +16,5 @@ public class BusinessModule extends AbstractGinModule {
         bind(ElasticSearchService.class).to(ElasticSearchServiceImpl.class);
 
         bind(ColorUtils.class);
-    }
-
-    @Provides
-    public ModelFactory getAutoBeanModelFactory() {
-        return ModelFactory.INSTANCE;
     }
 }
