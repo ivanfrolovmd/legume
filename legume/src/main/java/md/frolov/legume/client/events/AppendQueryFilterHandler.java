f7e7a2ce7509fb5d916412ec11837e985a5b518f
Ivan Frolov <ivan@frolov.md>
2013-06-02 14:36:48 +0300

    #4 Remove event

--- legume/src/main/java/md/frolov/legume/client/events/AppendQueryFilterHandler.java
+++
@@ -1,9 +0,0 @@
-package md.frolov.legume.client.events;
-
-import com.google.gwt.event.shared.EventHandler;
-
-/** @author Ivan Frolov (ifrolov@tacitknowledge.com) */
-public interface AppendQueryFilterHandler extends EventHandler
-{
-    void onAppendQueryFilter(AppendQueryFilter event);
-}
