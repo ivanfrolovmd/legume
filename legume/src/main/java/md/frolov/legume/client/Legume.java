74460eb4efb9b0be2941eeecb5e5c5c8ea9ce841
Ivan Frolov <ivan@frolov.md>
2013-06-04 06:53:38 +0300

    Uncaught exception handling.

--- legume/src/main/java/md/frolov/legume/client/Legume.java
+++ legume/src/main/java/md/frolov/legume/client/Legume.java
@@ -1,31 +1,44 @@
 package md.frolov.legume.client;
 
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 import md.frolov.legume.client.activities.stream.StreamPlace;
 import md.frolov.legume.client.elastic.query.SearchQuery;
+import md.frolov.legume.client.events.LogMessageEvent;
 import md.frolov.legume.client.gin.WidgetInjector;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
+/** Entry point classes define <code>onModuleLoad()</code>. */
 public class Legume implements EntryPoint
 {
-  private WidgetInjector injector = WidgetInjector.INSTANCE;
+    private static final Logger LOG = Logger.getLogger("Legume");
 
-  /**
-   * This is the entry point method.
-   */
-  public void onModuleLoad() {
-      injector.activityManager().setDisplay(injector.mainView());
-      RootLayoutPanel.get().add(injector.mainView());
+    private WidgetInjector injector = WidgetInjector.INSTANCE;
 
-      SearchQuery query = new SearchQuery("",null, new Date(), new Date());
-      injector.placeHistoryHandler().register(injector.placeController(), injector.eventBus(), new StreamPlace(query)); //TODO change to homeplace
-      injector.placeHistoryHandler().handleCurrentHistory();
-  }
+    /** This is the entry point method. */
+    public void onModuleLoad()
+    {
+        injector.activityManager().setDisplay(injector.mainView());
+        RootLayoutPanel.get().add(injector.mainView());
+
+        SearchQuery query = new SearchQuery("", null, new Date(), new Date());
+        injector.placeHistoryHandler().register(injector.placeController(), injector.eventBus(), new StreamPlace(query)); //TODO change to homeplace
+        injector.placeHistoryHandler().handleCurrentHistory();
+
+        GWT.setUncaughtExceptionHandler(new GWT.UncaughtExceptionHandler()
+        {
+            @Override
+            public void onUncaughtException(final Throwable e)
+            {
+                LOG.log(Level.SEVERE,"uncaught",e);
+                injector.eventBus().fireEvent(new LogMessageEvent("Uncaught error: "+e.getMessage()));
+            }
+        });
+    }
 
 }
