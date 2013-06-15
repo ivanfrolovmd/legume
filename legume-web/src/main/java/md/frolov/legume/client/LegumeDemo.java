2d6f7de309a960eb75e5da3b82f37d98ea95ba1d
Ivan Frolov <ivan@frolov.md>
2013-06-15 08:07:29 +0300

    Introduce appengine artifact.

---
+++ legume-web/src/main/java/md/frolov/legume/client/LegumeDemo.java
@@ -0,0 +1,11 @@
+package md.frolov.legume.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/** @author Ivan Frolov (ifrolov@tacitknowledge.com) */
+public class LegumeDemo implements EntryPoint
+{
+    public void onModuleLoad()
+    {
+    }
+}
