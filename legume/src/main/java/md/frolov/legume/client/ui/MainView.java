cebd4c77b73fa4ee916d512980a299d43a595dfa
Ivan Frolov <ivan@frolov.md>
2013-06-16 14:55:43 +0300

    UI hack to make context menu visible.

--- legume/src/main/java/md/frolov/legume/client/ui/MainView.java
+++ legume/src/main/java/md/frolov/legume/client/ui/MainView.java
@@ -3,11 +3,15 @@ package md.frolov.legume.client.ui;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
+
+import md.frolov.legume.client.ui.components.HeaderComponent;
 
 public class MainView extends Composite implements AcceptsOneWidget {
 
@@ -18,9 +22,13 @@ public class MainView extends Composite implements AcceptsOneWidget {
     
     @UiField
     Panel viewContainer;
+    @UiField
+    HeaderComponent header;
 
     public MainView() {
         initWidget(uiBinder.createAndBindUi(this));
+
+        DOM.setStyleAttribute((Element) header.getElement().getParentElement(), "overflow", "visible");
     }
 
     @Override
