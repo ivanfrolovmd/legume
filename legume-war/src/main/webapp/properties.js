640a56915ec8f5f82abdf1781e90522b3111c033
Ivan Frolov <ivan@frolov.md>
2013-07-19 07:48:57 +0300

    Raise timeout.

--- legume-war/src/main/webapp/properties.js
+++ legume-war/src/main/webapp/properties.js
@@ -1,6 +1,6 @@
 GlobalProperties = {
 	'elasticSearchServer': 'http://demo.logstash.net:9200',
-    'elasticSearchTimeout': 30000,
+    'elasticSearchTimeout': 60000,
     'pageSize': 100,
     'scrollThreasholdClientHeightRatio': 2
 }
